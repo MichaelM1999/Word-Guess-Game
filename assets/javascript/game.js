898e2942bdc3e36d62c0141aa34e5c98728f70b2
michael mariea <mariea.michael@gmail.com>
2019-06-05 21:34:12 -0700

    added jpg files and worked on conditional logic for js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+
 const words = {
     "OverlyDedicated": "Kendricks first album",
     "UntitledUnmastered": "unreleased album",
@@ -7,13 +10,30 @@ const words = {
     "Section.80": "Hii Power ADHD"
 }
 const randwordArr = ["OverlyDedicated", "UntitledUnmastered","ToPimPAButterFly","GoodKidM.A.A.D.City","DAMN.","Section.80"]
-const randword = randwordArr[Math.floor(Math.random() * randwordArr.length)]
+const randword = randwordArr[Math.floor(Math.random() * randwordArr.length)];
+const wins = 0;
+const answerArr = [];
+for (let i = 0; i < randword.length; i++) {
+    answerArr[i] = "_";
+    document.getElementById("answers").innerHTML = answerArr;
+}
+console.log(randword);
+console.log(answerArr);
+// on key up events
+const remainingletters = randword.length;
+console.log(remainingletters);
+    if (remainingletters > 0) {
+        var guess = event.key;
+        for (var j = 0; j < randword.length; j++) {
+            if (randword[j] === guess) {
+            answerArray[j] = guess;
+            remainingLetters--;
 
-let answerArr = []
-    for (let i = 0; i < randword.length; i++) {
-        answerArr[i] = "_";
-    }
-document.getElementById("answers").innerHTML = answerArr;
-console.log(randword)
-
-
+            console.log(guess);
+            console.log(remainingletters);
+            console.log(randword);
+            if (remainingletters === 0) {
+                getElementById("wins", wins++ );
+            }
+        }};
+}});
