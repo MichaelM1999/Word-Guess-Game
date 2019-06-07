040fc0be849406dbbbd846330c97a0f20d583b39
michael mariea <mariea.michael@gmail.com>
2019-06-06 18:09:07 -0700

    finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-
+// hints and words 
 const words = {
     "OverlyDedicated": "Kendricks first album",
     "UntitledUnmastered": "unreleased album",
@@ -9,9 +9,11 @@ const words = {
     "DAMN.": "Humble. D.N.A. Element.",
     "Section.80": "Hii Power ADHD"
 }
+//generating random word 
 const randwordArr = ["OverlyDedicated", "UntitledUnmastered","ToPimPAButterFly","GoodKidM.A.A.D.City","DAMN.","Section.80"]
 const randword = randwordArr[Math.floor(Math.random() * randwordArr.length)];
 const wins = 0;
+//making dashes for appropriate words 
 const answerArr = [];
 for (let i = 0; i < randword.length; i++) {
     answerArr[i] = "_";
@@ -19,21 +21,22 @@ for (let i = 0; i < randword.length; i++) {
 }
 console.log(randword);
 console.log(answerArr);
-// on key up events
+// using user guess 
 const remainingletters = randword.length;
 console.log(remainingletters);
+const guess = event.key;
     if (remainingletters > 0) {
-        var guess = event.key;
         for (var j = 0; j < randword.length; j++) {
-            if (randword[j] === guess) {
+            if (randword.includes(guess)) {
             answerArray[j] = guess;
             remainingLetters--;
+            }};
 
             console.log(guess);
             console.log(remainingletters);
             console.log(randword);
-            if (remainingletters === 0) {
-                getElementById("wins", wins++ );
-            }
-        }};
+            // wins and losses
+    if (remainingletters === 0) {
+        getElementById("wins", wins++);
+    };      
 }});
